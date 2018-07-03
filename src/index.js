eb9ca1fc17d48fa1f1b090e2ff913f50b9a38ac9
Pavol Rajzak <pavol.rajzak@itera.no>
2018-07-03 10:45:14 +0200

    React asynchronous handling
    In this commit you will see how the asynchronous fetch works in
    React and how to append it to your application.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import CustomAvatarGenerator from './CustomAvatarGenerator';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<CustomAvatarGenerator />, document.getElementById('root'));
 registerServiceWorker();
