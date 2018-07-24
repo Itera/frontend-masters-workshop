39f8880ae09e9e1470d0c153028da48498758962
Pavol Rajzak <pavol.rajzak@itera.no>
2018-07-24 10:46:22 +0200

    feat: add redux to application

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
+import rootReducer from './reducers';
 import App from './containers/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer);
+
+const ReduxApp = () => (
+    <Provider store={store}>
+        <App />
+    </Provider>
+)
+
+ReactDOM.render(<ReduxApp />, document.getElementById('root'));
 registerServiceWorker();
